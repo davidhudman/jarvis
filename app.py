49aef11981f0252a680c5b8a0fc32e3fba55d863
Bryan Overbey <bryanmorganoverbey@gmail.com>
2023-10-04 14:26:51 -0400

    make gpt mp3s

--- app.py
+++ app.py
@@ -1,33 +1,25 @@
+# Usage: python app.py "What is the OpenAI mission?"
+
 import openai
+import sys
+import os
 from gtts import gTTS
-tts = gTTS('hello my name is susan')
-tts.save('hello.mp3')
+
+# read input from the cli python
+input = sys.argv[1]
+print(input)
+
+# set the openai api key
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
-# completion = openai.ChatCompletion.create(
-#     model="gpt-3.5-turbo",
-#     messages=[{"role": "user", "content": "What is the OpenAI mission?"}]
-# )
-completion = {
-    "id": "chatcmpl-860ICXhpI2RDn6yFcubZQpohyDvky",
-    "object": "chat.completion",
-    "created": 1696441784,
-    "model": "gpt-3.5-turbo-0613",
-    "choices": [
-        {
-          "index": 0,
-          "message": {
-              "role": "assistant",
-              "content": "The OpenAI mission is to ensure that artificial general intelligence (AGI) benefits all of humanity. AGI refers to highly autonomous systems that outperform humans at most economically valuable work. OpenAI aims to build safe and beneficial AGI directly, but if another project comes close to building AGI before they do, they commit to stop competing and start assisting that project instead. Their primary focus is to ensure that AGI development is carried out in a safe, responsible, and cooperative manner to avoid harmful effects and maximize societal benefits."
-          },
-            "finish_reason": "stop"
-        }
-    ],
-    "usage": {
-        "prompt_tokens": 14,
-        "completion_tokens": 106,
-        "total_tokens": 120
-    }
-}
+completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": input}]
+)
 
-print(completion)
+# print the message from the completion object
+message = completion['choices'][0]['message']['content']
+print(message)
+tts = gTTS(message)
+tts.save("openai.mp3")
