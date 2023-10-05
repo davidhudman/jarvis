5b8de45cc610fd7d61b46c16dadf11772559b136
Bryan Overbey <bryanmorganoverbey@gmail.com>
2023-10-05 10:12:55 -0400

    play sound from mp3

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": input}]
+    messages=[{"role": "user", "content": "write a 30 word response to the following question: " + input}]
 )
 
 # print the message from the completion object
@@ -23,3 +23,11 @@ message = completion['choices'][0]['message']['content']
 print(message)
 tts = gTTS(message)
 tts.save("openai.mp3")
+
+# play sound
+file = "openai.mp3"
+print('playing sound using native player')
+os.system("afplay " + file + " -r 1.8")
+
+# delete the file
+os.remove(file)
